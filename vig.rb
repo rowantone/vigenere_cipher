e7ee545ba6b6b49f53b44f47d62a297a83f6c669
Rowan <50375391+rowantone@users.noreply.github.com>
2020-02-11 16:27:02 -0500

    attack -- finds key length, changing to add 256
    lots of bugs !!

--- vig.rb
+++ vig.rb
@@ -1,22 +1,31 @@
-require 'facets'
+# work on keys using "whatever".bytes conv
 
 def encrypt(string, keys)
     encrypted_word = ""
-    string.each_char.with_index do |char, i|
-        key = keys[i % keys.length]
-        index = LETTER_TO_INDEX[char]
-        encrypted_word << INDEX_TO_LETTER[(index + key) % 26] 
+
+    string.each_byte.with_index do |char, i|
+        key = (keys[i % keys.length]).ord
+        encrypted_word << (char+key)%256
+        puts "#{char} -- #{key} -- #{encrypted_word[i].ord}"
     end 
-    return encrypted_word.upcase
+    encrypted_word.each_char do |c|
+        puts c
+    end
+    puts "end encrypt"
+    return encrypted_word
 end 
 
 def decrypt_known(string, keys)
     decrypted_word = ""
     string.each_byte.with_index do |char, i|
-        key = keys[i % keys.length]
-        index = char-65
-        decrypted_word << INDEX_TO_LETTER[(index - key) % 26]
+        key = (keys[i % keys.length]).ord
+        decrypted_word << (char-key)%256
+        puts "#{char} -- #{key} -- #{decrypted_word[i].ord}"
     end 
+    decrypted_word.each_char do |c|
+        puts c
+    end
+    puts "end decrypt"
     return decrypted_word
 end 
 
@@ -26,35 +35,54 @@ def decrypt_unknown(string)
         array.each{|key| hash[key] += 1}
         return hash
     end
+    
     def coincidences(string)
-        original_string = string.dup
-        check_string = string.dup
-        coincidence_array = []
-        
-        # loops
-        (0...original_string.length).step(1) do |i|
-            coincidence_array[i] = 0
-            (0...check_string.length).step(1) do |j|
-                # puts "#{i} -- #{j}"
-                if original_string[i+j] == check_string[j]
-                    coincidence_array[i] += 1
-                else end
+        def gcd(distance_array)
+            array = []
+            for a in distance_array[0...10] do
+                array.insert(-1, a[0])
             end
-            check_string.chop!
+            p array
+            x = array.reduce(:gcd)
+            puts x
         end
-        # original_string.length.times do |i|
-        #     coincidence_array[i] = 0
-        #     (0...check_string.length).step(1) do |j|
-        #         # puts "#{i} -- #{j}"
-        #         if original_string[j] == check_string[j]
-        #             coincidence_array[i] += 1
-        #         else end
-        #     end
-        #     check_string = check_string.rotate(-1)
-        # end
-        p coincidence_array
-        coincidence_array.shift
-        p coincidence_array.max
+
+        check_string = string.dup
+        coincidence_hash = Hash.new
+        distance_hash = Hash.new
+
+        # loops
+        # find indexes of strings of length 3
+		(1...check_string.length-2).step(1) do |i|
+			grab = "#{check_string[i-1]}#{check_string[i]}#{check_string[i+1]}"
+            if coincidence_hash[grab] != nil
+                coincidence_hash[grab].insert(-1, i)
+            else
+                coincidence_hash[grab] = [i]
+            end
+        end
+
+        # find distance between indexes for each string
+        for a in  coincidence_hash do
+            if a[1].length > 1
+                (0...a[1].length-1).step(1) do |i|
+                    grab = (a[1][i] - a[1][i+1]).abs
+                    if distance_hash[grab] != nil
+                        distance_hash[grab] += 1
+                    else
+                        distance_hash[grab] = 0
+                    end
+                end
+            else
+                next
+            end
+        end
+        distance_hash = distance_hash.sort_by {|i,j| [-j, i]}
+        p distance_hash[0]
+        gcd(distance_hash.to_a)
+
+        # coincidence_hash = coincidence_hash.sort_by {|i,j| [-j.length, i]}
+        # p coincidence_hash
     end
     x = frequency(string.split(""))
     x = x.sort_by{|a,b|b}
@@ -67,9 +95,10 @@ INDEX_TO_LETTER = {0=>"a", 1=>"b", 2=>"c", 3=>"d", 4=>"e", 5=>"f", 6=>"g", 7=>"h
 LETTER_TO_INDEX = {"a"=>0,"b"=> 1,"c"=> 2,"d"=> 3,"e"=> 4,"f"=> 5,"g"=> 6,"h"=> 7,"i"=> 8,"j"=> 9, "k"=>10, "l"=>11, "m"=>12, "n"=>13, "o"=>14, "p"=>15, "q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20,"v"=>21, "w"=>22, "x"=>23, "y"=>24, "z"=>25}
 
 LETTER_FREQUENCY = {"e"=>12.702,"t"=>9.356,"a"=>8.167,"o"=>7.507,"i"=>6.966,"n"=>6.749,"s"=>6.327,"h"=>6.094,"r"=>5.987,"d"=>4.253,"l"=>4.025,"u"=>2.758,"w"=>2.560,"m"=>2.406,"f"=>2.228,"c"=>2.202,"g"=>2.015,"y"=>1.994,"p"=>1.929,"b"=>1.492,"k"=>1.292,"v"=>0.978,"j"=>0.153,"x"=>0.150,"q"=>0.095,"z"=>0.077}
-y = "itshouldbenotedthativeupvotedeverysinglepersonwhosdisagreedwithmehereasfarasiknowinthgradeitookansattestwithoutpreparingforitatallitwasspurofthemomentiknewaboutitaboutanhouraheadoftimeanddidntdoanyresearchoranythingiscoredhigheronitthantheaveragepersonusingittoapplyforcollegeinmyareaaniqtesthasshownmetobeinthethpercentileforiqthisisthehighestresultthetestiwasgivenreachesanythingfurtherandtheydconsiderittobewithinthemarginoferrorforthattestmymothersboyfriendofyearsisanaerospaceengineerwhograduatedvirginiatechattheageofiunderstandphysicsbetterthanhimandioweverylittleofittohimashewouldrarelygivemeadecentexplanationofanythingjusttellmethatmyideaswerewrongandbecomeaggravatedwithmefornotquiteunderstandingthermodynamicshesnotparticularlysuccessfulasanengineerbutivemetlotsofotherengineerswhoarentasgoodasmeatphysicssoimguessingthatsnotjustaresultofhimbeingbadatitimalsoprettygoodatengineeringidonthaveadegreeandotherthanphysicsidonthaveabetterunderstandingofanyaspectofengineeringthananyactualengineerbutihavelotsofingenuityforinventingnewthingsforexampleiindependentlyinventedregenerativebrakesbeforefindingoutwhattheywereandiwasonlysevenoreightyearsoldwhenistartedinventingwirelesselectricitysolutionsmyfirstideabeingtouseapowerfulinfraredlasertotransmitenergyadmittedlynotthebestplanihaveindependentlythoughtofbasicallyeverybranchofphilosophyivecomeacrosseveryquestionofexistentialismwhichiveseendiscussedinsmbcorxkcdorredditoranywhereelsethethoughtshaventbeennewtomephilosophyhasprettymuchgottentrivialformeiveconsideredtakingaphilosophycoursejusttoseehoweasyitispsychologyiactuallyunderstandbetterthanpeoplewithdegreesunlikeengineeringtheresnoaspectofpsychologywhichidonthaveaverygoodunderstandingoficandebunkmanyofevensigmundfreudstheoriesimagoodenoughwriterthatimwritingabookandsofareverybodywhosreadanyofithassaiditwasreallygoodandplausibletoexpecttohavepublishedandthatsnotjustlikemeandfamilymembersthatcountsstrangersontheinternetiveheardzeronegativeappraisalofitsofarpeoplehavecritiqueditbutnotinsulteditidontknowifthatwillsufficeasevidencethatimintelligentimdonewithitthoughbecauseidratherdefendmymaturitysinceitswhatyouvespentthemosttimeattackingthefollowingaresomeexamplesofmymoralsandethicalcodeibelievefirmlythateverybodydeservesafutureifweweretocapturehitlerattheendofwwiiiwouldbeagainstexecutinghiminfactifwehadanywayofrehabilitatinghimandknowingthathewasntjustfakingitidevensupporttheconceptoflettinghimgofreethisisessentiallybecauseithinkthatwhoeveryouareinthepresentisaseparateentityfromwhoyouwereinthepastandwhoyouareinthefutureandwhileyourpresentselfshouldtakeresponsibilityforyourpastselfsactionsitshouldntbepunishedforthemsimplyforthesakeofpunishmentespeciallyifthepresentselfregretstheactionsofthepastselfandfeelsgenuineguiltaboutthemidontbelieveinjudgementofpeoplebasedontheirpersonalchoicesaslongasthosepersonalchoicesarentharmingothersidonthaveanyissuewithanytypeofsexualitywhatsoevershortofphysicallyactingoutnecrophiliapedophiliaorotheractswhichhaveaharmfulaffectonothersbutidontcarewhatapersonsfantasiesconsistofaslongastheyrecognizethedifferencebetweenrealityandfictionandcanseparatethem"
-x = encrypt(y,[1,2,3,4,5,6,7,8,9,10])
+y = "i love my boyfriend"
+x = encrypt(y, "abcdcba")
 
 puts x
 
-decrypt_unknown(x)
+z = decrypt_known(x,"abcdcba")
+puts z
